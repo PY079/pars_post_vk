f45c571850ca6c134f24dd41c6692d91172b6e0b
Ratata <84903507+PY079@users.noreply.github.com>
2023-05-02 15:43:40 +0300

    Update zn.py

--- zn.py
+++ zn.py
@@ -4,12 +4,13 @@ from telebot.types import InputMediaPhoto
 from data_project import vk_token_ra, id_gossip_nf, id_gossip_nf_clone, tg_token_bot, id_tg_group
 
 
+
 def send_med_gr(sp_tg,cop,text_tg,id_post):
     media = []
 
     if len(sp_tg) >1:
         text_tg+= f"<a href='{cop}'>Оригинальный пост</a>\n<a href='https://vk.com/wall{id_gossip_nf_clone}_{id_post}'>Скопированный пост</a>\n\n<a href='https://vk.com/gossipnfclone'>Gossip NF CLONE</a>\n<a href='https://vk.com/gossipnf'>Gossip NF</a>"
-        for i in range(len(sp_tg)): 
+        for i in range(len(sp_tg)):
             if i == 0:
                 media.append(InputMediaPhoto(sp_tg[i], text_tg, parse_mode='HTML'))
             else:
@@ -19,22 +20,25 @@ def send_med_gr(sp_tg,cop,text_tg,id_post):
         text_tg+= f"<a href='{cop}'>Оригинальный пост</a>\n<a href='https://vk.com/wall{id_gossip_nf_clone}_{id_post}'>Скопированный пост</a>\n\n<a href='https://vk.com/gossipnfclone'>Gossip NF CLONE</a>\n<a href='https://vk.com/gossipnf'>Gossip NF</a>"
         bot.send_photo(id_tg_group, sp_tg[0], text_tg, parse_mode='HTML')
 
-
 def con(): # Создаём подключени
-    os.system('cls')
+    os.system('clear')
+    print('START')
     global vk, bot
     session = vk_api.VkApi(token=vk_token_ra)
     vk = session.get_api()
     bot = telebot.TeleBot(tg_token_bot)
+
 
 def upd_db():
     global asd
     wal = vk.wall.get(owner_id = id_gossip_nf, count = 1,offset = 0, filter = 'all')
 
     id_lat = wal['count']
+    #print(id_lat)
+
     id_ew = id_lat+1
 
-    pk = 'C:/Users/User/Desktop/tg_bot_mus/gho.db'
+    pk = 'C:/Users/днс/Desktop/vk_p/pv/gho.db'
     s= '/home/gygcywdvevev/pa/gho.db'
 
     connect = sqlite3.connect(pk)
@@ -43,6 +47,7 @@ def upd_db():
     connect.commit()
 
     count_post_bd = ew[0]
+
 
 
 
@@ -67,7 +72,7 @@ def upd_db():
         print(f'В БД -- {count_post_bd} поменял на -- {count_post_bd - new_id}')
         connect.commit()
 
-    
+
 
 
 def wall_get():
@@ -118,50 +123,69 @@ def wall_get():
     if 'text' in wa['response']['items'][0]:
         mes_text2 = wa['response']['items'][0]['text'] # Текст сообщения
         lower_text = mes_text2.lower()
-        block_list=['тогда тебе','записывайтесь','ждём вас','ждем вас','перейдите по ссылке','наш адрес', 'требуются','записывайся','записывайся по номеру','переходите по ссылке','предложение ограниченно','цены вырастут','заходите в сообщество','многое другое в паблике','самые низкие цены','низкие цены','переходи по ссылки в комментариях','все подробности в паблике','подробная информация в группе','качественно','недорого','качественно и не дорого','пишите и звоните',' подписывайся на канал',' подписывайся']
+        block_list=['тогда тебе','записывайтесь','ждём вас','ждем вас',
+            'перейдите по ссылке','наш адрес', 'требуются',
+            'записывайся','записывайся по номеру','переходите по ссылке',
+            'предложение ограниченно','цены вырастут','заходите в сообщество',
+            'многое другое в паблике','самые низкие цены','низкие цены',
+            'переходи по ссылки в комментариях','все подробности в паблике',
+            'подробная информация в группе','качественно','недорого',
+            'качественно и не дорого','пишите и звоните',' подписывайся на канал',
+            'подписывайся','спешите обновить','у нас вы найдете','сделайте заказ',
+            'сделайте заказ прямо сейчас','скидка']
         post_ads = any(ad in lower_text for ad in block_list)
-    
+
     cop = f"https://vk.com/wall{wa['response']['items'][0]['owner_id']}_{wa['response']['items'][0]['id']}"
-    
+
     if 'text' in wa['response']['items'][0] and not 'В посте найдено голосование' in text_tg:
         mes_text = wa['response']['items'][0]['text'] # Текст сообщения
-        
+
         if len(mes_text)>3966:
             text_tg+= 'Bad Request: message is too long. Max 4096\n\n-------------\n'
         else:
             text_tg+= f'{mes_text}\n'
         text_in_post = True
-    
+
     elif 'text' in wa['response']['items'][0] and 'В посте найдено голосование' in text_tg:
         answ =[]
         mes_text = wa['response']['items'][0]['text'] # Текст сообщения
-        
+
         if len(mes_text)>3966:
             text_tg+= 'Bad Request: message is too long. Max 4096\n\n-------------\n'
         else:
-        
+
             for i2 in wa['response']['items'][0]['attachments'][0]['poll']['answers']:
                 answ.append(i2['text'])
             text_tg+= f"Тема: {wa['response']['items'][0]['attachments'][0]['poll']['question']}\nВарианты:  {', '.join(answ)}\n\n-------------\n"
 
         text_in_post = True
     else: text_in_post = False
-    
+
 
     if copy_history == False and marked_as_ads == False and block_comments == False and post_ads == False:
         if 'attachments' in wa['response']['items'][0]:
             for i in wa['response']['items'][0]['attachments']:
                 _type = i['type']
-                
+
                 if _type == 'photo' or _type =='video':
-                    sp_vk.append(f"{_type}{i[_type]['owner_id']}_{i[_type]['id']}_{i[_type]['access_key']}")
-                    if _type == 'photo':
+                    print(1)
+
+                    if _type == 'photo' and 'album_id' in i['photo']:
                         sp_tg.append(f"https://vk.com/{_type}{i[_type]['owner_id']}_{i[_type]['id']}")
+                        sp_vk.append(f"{_type}{i[_type]['owner_id']}_{i[_type]['id']}_{i[_type]['access_key']}")
+                        print(f"{_type}{i[_type]['owner_id']}_{i[_type]['id']}_{i[_type]['access_key']}")
+
+
+                    if _type == 'photo' and not 'album_id' in i['photo']:
+                        sp_tg.append(f"https://vk.com/{_type}{i[_type]['owner_id']}_{i[_type]['id']}")
+                        sp_vk.append(f"{_type}{i[_type]['owner_id']}_{i[_type]['id']}_{i[_type]['access_key']}")
+                    
                     if _type == 'video':
                         sp_tg.append('https://vk.com/photo-214698143_457239023')
+                        sp_vk.append(f"{_type}{i[_type]['owner_id']}_{i[_type]['id']}_{i[_type]['access_key']}")
 
-                elif _type == 'poll':   
-                    poll = True  
+                elif _type == 'poll':
+                    poll = True
                     sp_vk.append(f"poll{i['poll']['owner_id']}_{i['poll']['embed_hash']}")
                     text_tg+= f"В посте найдено голосование\n"
 
@@ -176,29 +200,29 @@ def wall_get():
                     album = True
                     print(12)
                     sp_vk.append(f"{_type}{i[_type]['owner_id']}_{i[_type]['id']}")
-                    text_tg+= f"\nАльбом -->  <a href='https://vk.com/{_type}{i[_type]['owner_id']}_{i[_type]['id']}'>{i[_type]['title']}</a>\n"            
+                    text_tg+= f"\nАльбом -->  <a href='https://vk.com/{_type}{i[_type]['owner_id']}_{i[_type]['id']}'>{i[_type]['title']}</a>\n"
                     print(sp_vk)
 
                 elif _type == 'doc':
                     doc=True
                     sp_vk.append(i[_type]['url'].replace('https://vk.com/','').replace('?hash=','_').split('&')[0])
                     text_tg+=f"\nДокумент -->  <a href ='{i[_type]['url'].split('?')[0]}'>{i[_type]['title']}</a>\n"
-                
+
                 elif _type == 'link' and wa['response']['items'][0]['attachments'][0][_type]['caption'] == 'youla.ru':
                     youla=True
                     sp_vk.append(wa['response']['items'][0]['attachments'][0][_type]['url'])
                     text_tg+= f"Объявление --> <a href ='{i[_type]['url']}'>{i[_type]['title']}</a>\n"
-                
+
                 elif _type == 'link' and wa['response']['items'][0]['attachments'][0][_type]['description'] == 'Плейлист':
                     audio = True
                     url0 = wa['response']['items'][0]['attachments'][0][_type]['url'].replace('access_hash=','').replace('https://m.vk.com/audio?act=','').split('&')
                     t = wa['response']['items'][0]['attachments'][0][_type]['url']
-                    
+
                     sp_vk.append(f'{url0[0]}_{url0[1]}')
                     text_tg+= f"Плейлист --> <a href='{t}'>{wa['response']['items'][0]['attachments'][0][_type]['title']}</a>\n"
 
 
-                
+
 
         text_tg+='-------------\n'
         if 'geo' in wa['response']['items'][0]['attachments']:
@@ -206,7 +230,7 @@ def wall_get():
             la=hyt[0]; lo=hyt[1]; geo=True
         else: geo=False
 
-        
+
 
         if 'signer_id' in wa['response']['items'][0]:
             p = wa['response']['items'][0]['signer_id'] # Подпись
@@ -226,7 +250,7 @@ def wall_get():
             signer_id = True
 
         else: signer_id = False
-        
+
 
 
 
@@ -234,12 +258,12 @@ def wall_get():
             if geo == False:
                 id = vk.wall.post(owner_id = id_gossip_nf_clone, message=(f'{mes_text}\n\n--------------------\nОт: [id{p}|{first_name} {last_name}]'),attachments=sp_vk,  signed=0, copyright=cop)
                 if audio == False and poll == False and album == False and doc == False and youla == False:
-                    send_med_gr(sp_tg,cop,text_tg,id['post_id'])  
+                    send_med_gr(sp_tg,cop,text_tg,id['post_id'])
                 else:
                     text_tg+= f"<a href='{cop}'>Оригинальный пост</a>\n<a href='https://vk.com/wall{id_gossip_nf_clone}_{id['post_id']}'>Скопированный пост</a>\n\n<a href='https://vk.com/gossipnfclone'>Gossip NF CLONE</a>\n<a href='https://vk.com/gossipnf'>Gossip NF</a>"
                     bot.send_message(id_tg_group,text_tg, parse_mode='HTML',disable_web_page_preview = True)
                 print('пост 1')
-            
+
             if geo == True:
                 vk.wall.post(owner_id = id_gossip_nf_clone, message=(f'{mes_text}\n\n--------------------\nОт: [id{p}|{first_name} {last_name}]'),attachments=sp_vk,  signed=0, copyright=cop, lat=la, long=lo)
                 print('пост 1')
@@ -251,24 +275,24 @@ def wall_get():
                 text_tg+= f"<a href='{cop}'>Оригинальный пост</a>\n<a href='https://vk.com/wall{id_gossip_nf_clone}_{id['post_id']}'>Скопированный пост</a>\n\n<a href='https://vk.com/gossipnfclone'>Gossip NF CLONE</a>\n<a href='https://vk.com/gossipnf'>Gossip NF</a>"
                 bot.send_message(id_tg_group,text_tg, parse_mode='HTML',disable_web_page_preview = True)
                 print('пост 2')
-            
+
             if geo == True:
                 vk.wall.post(owner_id = id_gossip_nf_clone, message=(f'{mes_text}\n\n--------------------\nОт: [id{p}|{first_name} {last_name}]'),  signed=0, copyright=cop, lat=la, long=lo)
                 print('пост 2')
-        
+
 
         if len(sp_vk)>0 and text_in_post == True and signer_id == False:
             if geo == False:
                 id = vk.wall.post(owner_id = id_gossip_nf_clone, message=mes_text,attachments=sp_vk,  signed=0, copyright=cop)
-                
-                if audio == False and poll == False and album == False and doc == False: send_med_gr(sp_tg,cop,text_tg,id['post_id'])    
-                
-                else: 
+
+                if audio == False and poll == False and album == False and doc == False: send_med_gr(sp_tg,cop,text_tg,id['post_id'])
+
+                else:
                     text_tg+= f"<a href='{cop}'>Оригинальный пост</a>\n<a href='https://vk.com/wall{id_gossip_nf_clone}_{id['post_id']}'>Скопированный пост</a>\n\n<a href='https://vk.com/gossipnfclone'>Gossip NF CLONE</a>\n<a href='https://vk.com/gossipnf'>Gossip NF</a>"
                     bot.send_message(id_tg_group,text_tg, parse_mode='HTML',disable_web_page_preview = True)
-                
+
                 print('пост 3')
-            
+
             if geo == True:
                 vk.wall.post(owner_id = id_gossip_nf_clone, message=mes_text,attachments=sp_vk,  signed=0, copyright=cop, lat=la, long=lo)
                 print('пост 3')
@@ -276,9 +300,14 @@ def wall_get():
         if len(sp_vk)>0 and text_in_post == False and signer_id == False:
             if geo == False:
                 id = vk.wall.post(owner_id = id_gossip_nf_clone, attachments=sp_vk,  signed=0, copyright=cop)
-                send_med_gr(sp_tg,cop,text_tg,id['post_id'])  
+                if audio == False and poll == False and album == False and doc == False:
+                    send_med_gr(sp_tg,cop,text_tg,id['post_id'])
+                else:
+                    text_tg+= f"<a href='{cop}'>Оригинальный пост</a>\n<a href='https://vk.com/wall{id_gossip_nf_clone}_{id['post_id']}'>Скопированный пост</a>\n\n<a href='https://vk.com/gossipnfclone'>Gossip NF CLONE</a>\n<a href='https://vk.com/gossipnf'>Gossip NF</a>"
+                    bot.send_message(id_tg_group,text_tg, parse_mode='HTML',disable_web_page_preview = True)
+
                 print('пост 4')
-            
+
             if geo == True:
                 vk.wall.post(owner_id = id_gossip_nf_clone, attachments=sp_vk,  signed=0, copyright=cop, lat=la, long=lo)
                 print('пост 4')
@@ -288,15 +317,24 @@ def wall_get():
 
                 id = vk.wall.post(owner_id = id_gossip_nf_clone, message=mes_text,  signed=0, copyright=cop)
 
-                text_tg+= f"<a href='{cop}'>Оригинальный пост</a>\n<a href='https://vk.com/wall-219505503_{id['post_id']}'>Скопированный пост</a>\n\n<a href='https://vk.com/gossipnfclone'>Gossip NF CLONE</a>\n<a href='https://vk.com/gossipnf'>Gossip NF</a>"
+                text_tg+= f"<a href='{cop}'>Оригинальный пост</a>\n<a href='https://vk.com/wall-214698143_{id['post_id']}'>Скопированный пост</a>\n\n<a href='https://vk.com/gossipnfclone'>Gossip NF CLONE</a>\n<a href='https://vk.com/gossipnf'>Gossip NF</a>"
                 bot.send_message(id_tg_group,text_tg, parse_mode='HTML',disable_web_page_preview = True)
                 print('пост 5')
-                
-                
+
+
             if geo == True:
                 vk.wall.post(owner_id = id_gossip_nf_clone, message=mes_text,  signed=0, copyright=cop, lat=la, long=lo)
                 print('пост 5')
 
+def dp_c():
+    pk = 'C:/Users/User/Desktop/tg_bot_mus/gho.db'
+    s= '/home/gygcywdvevev/pa/gho.db'
+    connect = sqlite3.connect(s)
+    cursor = connect.cursor()
+
+    id=6771
+    cursor.execute(f"UPDATE `name` SET `new_id`={id}")
+    connect.commit()
 
 
 def tg():
@@ -304,8 +342,11 @@ def tg():
     while True:
         upd_db()
         time.sleep(60)
+#dp_c()
+t()
 
-tg()
+
+
 # con()
 # print(upd_db())
 #wall_get()
